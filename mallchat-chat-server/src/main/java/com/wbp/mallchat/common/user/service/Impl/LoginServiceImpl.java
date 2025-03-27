101fffee7d1d3c9742323b587137e7cfe7289160
wangbp <798696556@qq.com>
2025-03-27 23:05:53 +0800

    生成token

--- mallchat-chat-server/src/main/java/com/wbp/mallchat/common/user/service/Impl/LoginServiceImpl.java
+++ mallchat-chat-server/src/main/java/com/wbp/mallchat/common/user/service/Impl/LoginServiceImpl.java
@@ -1,12 +1,28 @@
 package com.wbp.mallchat.common.user.service.Impl;
 
 import com.wbp.mallchat.common.user.service.LoginService;
+import com.wbp.mallchat.common.utils.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LoginServiceImpl implements LoginService {
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @Override
+    public void renewalTokenIfNecessary(String token) {
+
+    }
+
     @Override
     public String login(Long uid) {
-        return "";
+        String token = jwtUtils.createToken(uid);
+        return token;
+    }
+
+    @Override
+    public Long getValidUid(String token) {
+        return 0L;
     }
 }
