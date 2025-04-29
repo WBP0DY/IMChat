a76c36969e25f8e43bfd51f6b9c8e25bfd579fd3
wangbp <798696556@qq.com>
2025-04-29 23:56:43 +0800

    全局异常处理

---
+++ mallchat-chat-server/src/main/java/com/wbp/mallchat/common/user/service/IUserService.java
@@ -0,0 +1,16 @@
+package com.wbp.mallchat.common.user.service;
+
+import com.wbp.mallchat.common.user.domain.entity.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 用户表 服务类
+ * </p>
+ *
+ * @author wbp
+ * @since 2025-04-06
+ */
+public interface IUserService extends IService<User> {
+
+}
